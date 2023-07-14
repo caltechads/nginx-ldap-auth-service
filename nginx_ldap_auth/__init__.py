4364104e9ebe5141f3036ba1ce48917c9dfb489f
Chris Malek <cmalek@caltech.edu>
2023-07-14 10:39:18 -0700

    Bump version: 2.0.3 → 2.0.4

--- nginx_ldap_auth/__init__.py
+++ nginx_ldap_auth/__init__.py
@@ -1,4 +1,4 @@
-__version__: str = "2.0.3"
+__version__: str = "2.0.4"
 __author__: str = "Caltech IMSS ADS"
 __author_email__: str = "imss-ads-staff@caltech.edu"
 __description__: str = "A FastAPI app that authenticates users via LDAP and sets a cookie for nginx"
