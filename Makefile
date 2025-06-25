4105a4ba341dd73a59cfc54cbf53995ac429fa29
Chris Malek <cmalek@caltech.edu>
2025-06-25 10:41:24 -0700

    Bump version: 2.1.7 → 2.1.8

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION = 2.1.7
+VERSION = 2.1.8
 
 PACKAGE = nginx-ldap-auth-service
 
--- doc/source/conf.py
+++ doc/source/conf.py
@@ -36,7 +36,7 @@ copyright: str = "2023, Caltech IMSS ADS"  # noqa: A001
 author: str = "Caltech IMSS ADS"
 
 # The full version, including alpha/beta/rc tags
-release: str = "2.1.7"
+release: str = "2.1.8"
 
 
 # -- General configuration ---------------------------------------------------
